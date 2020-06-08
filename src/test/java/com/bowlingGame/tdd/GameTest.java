7f5a05d43d6deef0b77c9555110c88266f461867
yamorales <yanigmorales@gmail.com>
2020-06-08 20:22:32 -0300

    create setup

--- src/test/java/com/bowlingGame/tdd/GameTest.java
+++ src/test/java/com/bowlingGame/tdd/GameTest.java
@@ -1,28 +1,25 @@
 package com.bowlingGame.tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GameTest {
-    @Test
-    public void CanMakeGame(){
-        new Game();
-    }
+    //No es buena practica compartir instancias entre test, por eso usamos un setup
+    //Game game = new Game();
 
-    @Test
-    public void canRollBall(){
+    Game game;
+    @BeforeEach
+    void setUp() {
         Game game = new Game();
-        game.roll(0);
     }
 
     @Test
     void canScoreGutterGame() {
-        Game game = new Game();
         assert game.score() == 0;
     }
 
     @Test
     void canScoreGame() {
-        Game game = new Game();
         for (int score = 0; score < 20; score++) {
             game.roll(1);
         }
