4c0da41dba2fc59e10db75b41c385fbb232047a5
yamorales <yanigmorales@gmail.com>
2020-06-09 16:18:23 -0300

    refactor de test, reutilizacion de codigo

--- src/test/java/com/bowlingGame/tdd/GameTest.java
+++ src/test/java/com/bowlingGame/tdd/GameTest.java
@@ -1,29 +1,37 @@
 package com.bowlingGame.tdd;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class GameTest {
     //No es buena practica compartir instancias entre test, por eso usamos un setup
     //Game game = new Game();
 
-    Game game;
-    @BeforeEach
-    void setUp() {
-        Game game = new Game();
+    private Game game;
+    @BeforeAll
+    public void setUp() {
+        game = new Game();
     }
 
     @Test
     void canScoreGutterGame() {
+        game.score();
+        roll(0, 20);
         assert game.score() == 0;
     }
 
     @Test
     void canScoreGame() {
-        for (int score = 0; score < 20; score++) {
-            game.roll(1);
-        }
+        roll(1, 20);
         assert game.score() == 20;
+    }
 
+    private void roll(int pinsDown, int time) {
+        for (int i = 0; i < time; i++) {
+            game.roll(pinsDown);
+        }
     }
 }
